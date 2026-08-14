54162143590e97fdadce2c0a980d05cd1d23b8e5
Neha Singh <nehaypsingh2020@gmail.com>
2026-08-14 21:54:33 +0530

    Converting v3 tailwind to v4 tailwind syntax in global.css

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import { NAVBAR_HEIGHT } from '@/lib/constants'
+import Navbar from '../../components/Navbar'
+
+import React from 'react'
+
+const layout = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div>
+      <Navbar/>
+      <main className={`h-full flex w-full flex-col pt-[${NAVBAR_HEIGHT}px]`}>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default layout
